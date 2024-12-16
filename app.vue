6c9e72d7a3b873b02fa72f7560b43fa055652b7d
Heitor <heitorschleder33@gmail.com>
2024-12-16 18:15:14 -0300

    first commit

---
+++ app.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+useHead({
+  title: "Heitor Schleder",
+  link: [
+    { rel: 'icon', href: '/favicon.ico'},
+    { rel: "stylesheet", href: "https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@latest/dist/tabler-icons.min.css" },]
+
+});
+</script>
+<template>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
---
+++ pages/main/index.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>ola mundinho</h1>
+</template>
